5b7f1743bfa9a62db75ac2177f579052085f5299
Frankie <dracko@hotmail.co.uk>
2020-06-08 08:33:16 +0000

    Describe the check the age

---
+++ spec/ageSpec.js
@@ -0,0 +1,8 @@
+describe("whatCanIDrink", function() {
+
+    describe("Check the age", function() {
+        it("should return 'Sorry.I can't tell what drink because that age is incorrect!'"), function(){
+            expect(age.value).toBe(0);
+        });
+    });
+});
