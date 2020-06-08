aa6f7bbee624f8901dc28cf37052e6a1b341e395
Frankie <dracko@hotmail.co.uk>
2020-06-08 20:26:48 +0000

    beforeEach added to ageSpec

--- spec/ageSpec.js
+++ spec/ageSpec.js
@@ -1,4 +1,8 @@
 describe("Drink About", function(){
+    beforeEach(function() {
+        age = new whatCanIDrink;
+    });
+
     describe("Check the age", function() {
         it("should return \"Drink Toddy\"", function() {
             expect(age).toBe(14);
